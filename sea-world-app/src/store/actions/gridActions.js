954c4f96d23457f47045cfcfd1c455709e61a79f
naldoreupo <nreupo@idat.edu.pe>
2021-10-10 20:24:33 -0500

    Fix sintax

--- sea-world-app/src/store/actions/gridActions.js
+++ sea-world-app/src/store/actions/gridActions.js
@@ -1,13 +1,6 @@
-
-
 export const UPDATE_DIMENSION_MATRIX = "UPDATE_DIMENSION_MATRIX";
 export const UPDATE_DIMENSION_MATRIX_SUCCESS = "UPDATE_DIMENSION_MATRIX_SUCCESS";
 export const UPDATE_CELL_MATRIX = "UPDATE_CELL_MATRIX";
-
-/* export const updateDimensionMatrix = (payload) => ({
-      type: UPDATE_DIMENSION_MATRIX,
-      payload: payload
-}); */
 
 export const updateDimensionMatrixSuccess = (payload) => ({
       type: UPDATE_DIMENSION_MATRIX,
@@ -25,9 +18,9 @@ export const updateDimensionMatrix = (payload) => {
 
             let matrix = [];
 
-            [...Array(payload.rows)].map((row, indexRow) => {
+            [...Array(payload.rows)].forEach((row, indexRow) => {
                   let tempArray = [];
-                  [...Array(payload.columns)].map((column, indexColumn) => {
+                  [...Array(payload.columns)].forEach((column, indexColumn) => {
                         tempArray.push(
                               {
                                     "key": indexRow + "-" + indexColumn,
